e5ca7e4c556f13ddcc9b604e81e92137388450f7
RaulGM <raulgm83@gmail.com>
2016-01-10 15:53:34 +0100

    great advances

--- client/app/app.js
+++ client/app/app.js
@@ -87,20 +87,20 @@ var options = [
 var portfolio = [
   {
     title:                  "RaulGM-Quiz!'", 
-    image:                  "../../assets/images/webPhotographyExample.png", 
+    image:                  "../../assets/images/RGMQ.png", 
     text:                   "Proyecto full-stack de final de curso realizado en 2015 de node.js a través del portal MiriadaX. En él, se podrán crear, borrar, comentar y contestar preguntas. Dispone de un login y de un autologout.", 
     url:                    "https://raulgmquiz.herokuapp.com/"
   },
   {
     title:                  "De Moratalaz al 'Zielo'", 
-    image:                  "../../assets/images/webPhotographyExample.png", 
-    text:                   "Web que habla de la historia, lugares y comercios del barrio madrileño de Moratalaz. Esta web ha sido desarrollada con WordPress, con la peculiaridad de que los estilos, el tema, las páginas han sido desarrollados desde cero por mí. No me he basado en ningún tema en particular, ya que este ha sido mi proyecto personal para aplicar todo lo aprendido con WordPress en estos últimos meses. Como se puede apreciar la web es totalmente responsive", 
+    image:                  "../../assets/images/DMAZ.png", 
+    text:                   "Blog que habla de la historia, lugares y comercios del barrio de Moratalaz. Desarrollada con WordPress con los estilos, el tema y las páginas desde cero por mí.", 
     url:                    "http://dmaz.raul-gm.com/"
   },
   {
     title:                  "Rock this concert", 
-    image:                  "../../assets/images/webPhotographyExample.png", 
-    text:                   "App de Android que muestra todos los conciertos de Rock y Heavy que se dan en España. En una pestaña se visualizarán de manera sencilla y directa los conciertos que se celebrarán en dicha semana. En la siguiente pestaña se tendrá al alcance de la mano todos los conciertos que se celebrarán, pudiendo filtrar por ciudad, grupo y/o fecha. Por último, se mostrarán tus conciertos favoritos. Estos serán añadidos por el usuario en las opciones del propio concierto. Además se podrán añadir a tu calendario de Google Maps, e incluso acceder a la información detallada de la sala donde se celebrará el show.", 
+    image:                  "../../assets/images/AppRtC.png", 
+    text:                   "App de Android que muestra todos los conciertos de Rock y Heavy que se dan en España. Todos los conciertos  del año al alcance de tu mano. Acceso directo a los que se van a celebrar en dicha semana. Guarda tus conciertos en fávoritos. Consulta los datos del concierto y ubica la sala donde se va a celebrar.", 
     url:                    "https://play.google.com/store/apps/details?id=es.raulgm.rockthisconcert"
   },
   {
